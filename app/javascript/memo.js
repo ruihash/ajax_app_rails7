a1c8c17221506c2e91acd0d31446a874d622a412
ruihash <rstoyo.yuta.hashiura@gmail.com>
2023-12-14 14:41:56 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,21 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+       投稿日時:${item.created_at}
+      </div>
+      <div class="post-content">
+      ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
+
+
+
+
 function post (){
   
   const form = document.getElementById("form") ;
@@ -8,7 +26,31 @@ function post (){
    XHR.open("POST", "/posts", true);
    XHR.responseType = "json";
    XHR.send(formData);
+   XHR.onload = () => {
+    if (XHR.status !=200){
+      alert(`Error ${XHR.status}: ${XHR.statusText}`);
+      return null;
+    }/*エラーが出た際にエラーかどうかどうか分かるようアラームを設定 */
+    const list =document.getElementById("list")
+    const formTExt = document.getElementById("content");
+   /*
+    const item = XHR.response.post;
+    const html = `
+    <div class="post">
+      <div class="post-tate">
+        投稿日時:${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+   list.insertAdjacentHTML("afterend", html)
+   */
+  list.insertAdjacentHTML("afterend", buildHTML(XHR));
+   formText.value = "";
+   };
   });
 };
 
 window.addEventListener('turbo:load', post);
+
